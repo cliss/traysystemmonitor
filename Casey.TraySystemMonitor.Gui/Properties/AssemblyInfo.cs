54332b16e1591c986f2e969335c2eb960142254b
Sören Kuklau <kuklau@trenz.de>
2020-05-22 22:21:52 +0200

    modernize csproj files

--- Casey.TraySystemMonitor.Gui/Properties/AssemblyInfo.cs
+++ Casey.TraySystemMonitor.Gui/Properties/AssemblyInfo.cs
@@ -2,22 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Casey.TraySystemMonitor.Gui")]
-#if DEBUG
-[assembly: AssemblyDescription("Application to monitor your system resources.  Debug build.")]
-#else
-[assembly: AssemblyDescription("Application to monitor your system resources.  Release build.")]
-#endif
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Casey Liss")]
-[assembly: AssemblyProduct("Casey.TraySystemMonitor.Gui")]
-[assembly: AssemblyCopyright("Copyright © Casey Liss 2007-2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -25,13 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d68d878c-8d3f-4a91-9c8d-236f0e64d0ca")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
